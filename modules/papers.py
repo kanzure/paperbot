db89d5626467d5a67dad78c10d1d8049f5f514b5
Bryan Bishop <kanzure@gmail.com>
2013-07-05 22:08:16 -0500

    use full paths to modules in phenny

--- modules/papers.py
+++ modules/papers.py
@@ -8,7 +8,7 @@ import random
 import requests
 import lxml.etree
 from StringIO import StringIO
-import scihub
+import modules.scihub
 
 import pdfparanoia
 
@@ -136,10 +136,10 @@ def download(phenny, input, verbose=True):
                 phenny.say(download_url(line))
                 continue
         else:
-            pdfurl, doi = scihub.scihubber(line, headers={})
+            pdfurl, doi = modules.scihub.scihubber(line, headers={})
             if pdfurl:
                 if str.find(pdfurl, "pdfcache"):
-                    if doi: phenny.say(scihub.libgen(pdfurl, doi, headers={}))
+                    if doi: phenny.say(modules.scihub.libgen(pdfurl, doi, headers={}))
                     else: phenny.say(download_url(pdfurl))
                 else: phenny.say(pdfurl)
     return
